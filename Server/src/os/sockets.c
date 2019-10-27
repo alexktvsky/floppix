5dc658db6ac594cc99ae7035e29649d1ae6712a2
user <user@localhost>
2019-10-27 14:52:28 +0700

    Fix macro syntax

--- Server/src/os/sockets.c
+++ Server/src/os/sockets.c
@@ -17,6 +17,8 @@ int tcp_nopush(socket_t socket)
     int tcp_nopush = 1;
     return setsockopt(socket, IPPROTO_TCP, TCP_NOPUSH,
                      (const void *) &tcp_nopush, sizeof(int));
+#elif (SYSTEM_SOLARIS)
+    /* do smth */
 #else
     return 0;
 #endif
@@ -33,6 +35,8 @@ int tcp_push(socket_t socket)
     int tcp_nopush = 0;
     return setsockopt(socket, IPPROTO_TCP, TCP_NOPUSH,
                      (const void *) &tcp_nopush, sizeof(int));
+#elif (SYSTEM_SOLARIS)
+    /* do smth */
 #else
     return 0;
 #endif
@@ -44,6 +48,8 @@ int close_socket(socket_t socket)
 #if (SYSTEM_LINUX)
     return close(socket);
 #elif (SYSTEM_FREEBSD)
+    return close(socket);
+#elif (SYSTEM_SOLARIS)
     return close(socket);
 #elif (SYSTEM_WIN32) || (SYSTEM_WIN64)
     return closesocket(socket);
